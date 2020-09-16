4da064111e24b2e1f8c4d58b342daecce3ca12b8
Tibo Baguette <tibosid@hotmail.be>
2020-09-16 11:30:48 +0200

    tipCalculator with template strings

--- JavascriptOefeningen/tipCalculator.js
+++ JavascriptOefeningen/tipCalculator.js
@@ -1,6 +1,6 @@
 let tipCalculator = function(total, tipPercantage = 0.1) {
-    return total * tipPercantage
+    return `A ${tipPercantage * 100}% tip on $${total} would be ${total * tipPercantage}`
 }
 
 console.log(tipCalculator(100))
-console.log(tipCalculator(100, 0.2))
+console.log(tipCalculator(200, 0.2))
