e315afa82771881fe826b6c86d909e3c5b52de77
Tibo Baguette <tibosid@hotmail.be>
2020-09-21 14:05:16 +0200

    chapter 7

--- script.js
+++ script.js
@@ -15,27 +15,67 @@ const todoList = [{
     completed: true,
 }]
 
-// get uncompleted todos and print amount in new <p>
-let uncompletedCount = 0
-todoList.forEach(function(item) {
-    if (!item.completed) {
-        uncompletedCount++
-    }
+const filters = {
+    searchText: '',
+    hideCompleted: false,
+}
+
+const renderTodos = function(todos, filters) {
+    const filteredTodos = todos.filter(function(todo) {
+        if (filters.hideCompleted === true) {
+            return (todo.text.toLowerCase().includes(filters.searchText.toLowerCase()) &&
+                    todo.completed === false)
+        } else {
+            return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+        }
+    })
+
+    // clear div
+    document.querySelector('#todo-item-list').innerHTML = ''
+
+    // get uncompleted todos and print amount in new <p>
+    const uncompletedTodos = filteredTodos.filter(function(todo) {
+        return !todo.completed
+    })
+
+    const newParagraph = document.createElement('h2')
+    newParagraph.textContent = `You have ${uncompletedTodos.length} todos left`
+    document.querySelector('#todo-item-list').appendChild(newParagraph)
+
+    // print all todos in a new <p>
+    filteredTodos.forEach(function(item) {
+        const newParagraph = document.createElement('p')
+        newParagraph.textContent = item.text
+        document.querySelector('#todo-item-list').appendChild(newParagraph)
+    })
+}
+
+const addTodo = function(text, status) {
+    todoList.push({
+        text: text,
+        status: status
+    })
+}
+
+// render todos
+renderTodos(todoList, filters)
+
+// search input event listener
+document.querySelector('#search-todo-input').addEventListener('input', function(e) {
+    filters.searchText = e.target.value
+    renderTodos(todoList, filters)
 })
 
-const newParagraph = document.createElement('h2')
-newParagraph.textContent = `You have ${uncompletedCount} todos left`
-document.querySelector('body').appendChild(newParagraph)
-
-// print all todos in a new <p>
-todoList.forEach(function(item) {
-    const newParagraph = document.createElement('p')
-    newParagraph.textContent = item.text
-    document.querySelector('body').appendChild(newParagraph)
+// add input event listener
+document.querySelector('#form').addEventListener('submit', function(e) {
+    e.preventDefault()
+    addTodo(e.target.elements.addTodo.value, false)
+    renderTodos(todoList, filters)
+    e.target.elements.addTodo.value = ''
 })
 
-
-// button onClickListener
-document.querySelector('button').addEventListener('click', function(e) {
-    console.log('Did this work?')
+// hide completed checkbox event listener
+document.querySelector('#hide-completed').addEventListener('change', function(e) {
+    filters.hideCompleted = e.target.checked
+    renderTodos(todoList, filters)
 })
